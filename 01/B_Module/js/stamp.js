97f08b89b8654339aa40b5118d2a6741098e2597
taeyeon051 <y2010112@y-y.hs.kr>
2021-08-23 16:21:33 +0900

    B-Module

---
+++ 01/B_Module/js/stamp.js
@@ -0,0 +1,18 @@
+const log = console.log;
+
+window.onload = () => {
+    const stamp = new Stamp();
+}
+
+class Stamp {
+    constructor() {
+        
+
+        this.init();
+        this.addEvent();
+    }
+
+    addEvent() {
+        
+    }
+}
